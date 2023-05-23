fb9d620717029dc9bb2c8691777e1b67996f3106
Raymart-Leyson <raymart.leyson@cit.edu>
2023-05-23 17:25:31 +0800

    Create Problem-Generator App

---
+++ problem-generator/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="ProblemGenerator" >
+      <div className="mainUI">
+        <input type="text" className="generate">
+        </input>
+        <button className="btn_generate">Generate</button>
+        <ul>
+        <li>
+          <div className="question">All Finite Language are turing decidable.</div>
+          <div className="choice">TRUE or FALSE</div>
+        </li>
+        <li>
+          <div className="question">All turing decidable are turing acceptable.</div>
+          <div className="choice">TRUE or FALSE</div>
+        </li>
+        <li>
+          <div className="question">(ababb) is an element of L = &#123;(a+b)&#125;*aa+bb&#125;</div>
+          <div className="choice">TRUE or FALSE</div>
+        </li>
+        <li>
+          <div className="question">For any language L, e is an element of L+.</div>
+          <div className="choice">TRUE or FALSE</div>
+        </li>
+        </ul>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
