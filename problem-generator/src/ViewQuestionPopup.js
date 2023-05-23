2b1dd4f4d2a9fcf6e7de17e65957be65eb3e9e40
Hareram Chua <hareram.chua@cit.edu>
2023-05-24 03:32:59 +0800

    Modify ViewQuestionPopup and its design

--- problem-generator/src/ViewQuestionPopup.js
+++ problem-generator/src/ViewQuestionPopup.js
@@ -6,6 +6,7 @@ function ViewQuestionPopup({ question, choice, onClose }) {
       <div className="popup-content">
         <div>
           <h2>{question}</h2>
+          <br></br><br></br>
           <ul>
             {choice.map((option, index) => (
               <li key={index} className=' choices'>{option}</li>
@@ -15,6 +16,7 @@ function ViewQuestionPopup({ question, choice, onClose }) {
         <button className="close-popup" onClick={onClose}>
           Close
         </button>
+        <br></br>
       </div>
     </div>
   );
